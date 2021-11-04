689cd9d27112d1ad4f2a46115ac1568127bf95a9
Miguel Ángel Agudelo Lopera <32880967+Miguel22agudelo@users.noreply.github.com>
2021-11-04 12:08:26 -0500

    Clase 76 terminada

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import GifGridItem from "./GifGridItem";
 
 const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => getGifs(), []);
+
   const getGifs = async () => {
     const url = `https://api.giphy.com/v1/gifs/search?q=Messi&limit=10&api_key=Hzt0MYcdIYpENoNexZY6eHIU1R8pHy1U`;
     const resp = await fetch(url);
-    const {data} = await resp.json();
-    
-    const gifs = data.map(img => {
-        return{
-            id: img.id,
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }
-    })
-    
-    console.log(gifs);
-  };
+    const { data } = await resp.json();
 
-  getGifs()
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+
+    // console.log(gifs);
+    setImages(gifs);
+  };
 
   return (
     <div>
       <h3>{category}</h3>
+      {images.map((img) => (
+        <GifGridItem key={img.id} {...img} />
+      ))}
     </div>
   );
 };
